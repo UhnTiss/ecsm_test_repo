3a0bf634cd70404e388f7b3eb1cb363b452c3334
julian <julian.anthes@me.com>
2023-05-21 17:48:49 +0200

    added function to refactor

--- main.py
+++ main.py
@@ -20,6 +20,22 @@ def method_9(a, b):
     result = multiply(b, a)
     return result
 
+def to_refactor(number_1, number_2):
+    result = 0
+
+    for _ in range(method_4(number_1, number_2)):    
+        if method_6(result, number_1) > number_2:
+            number_3 = method_4(result, number_1) + method_9(number_2, number_1)
+        if method_4(result, number_2) < number_1:
+            number_3 = method_9(number_2, result)
+        else:
+            number_3 = method_4(result, number_2) + method_9(number_2, number_1)
+        result += method_7(result, number_3)
+    
+    result = method_9(number_2, number_1) - method_6(result, number_1)
+
+    return result
+
 def main():
     print("hello world!")
 
