028b5db5252052486b73e704449c0456b8f43d4d
julian <julian.anthes@me.com>
2023-05-21 17:03:30 +0200

    added multiple functions

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 def function_0():
     ...
 
+def function_1():
+    return None
+
+def function_2(a):
+    return a + a
+
+def function_3(a, b):
+    return a * b
+
+def function_4(a, b):
+    return a - b
+
+def function_5(a):
+    return a**2
+
+def function_6(a, b):
+    return a**b
+
+def function_7(a, b):
+    return a / b
+
+def function_8(a, b):
+    return b / a
+
+def function_9(a, b):
+    return b * a
+
 def main():
     print("hello world!")
 
