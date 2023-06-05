9591cb7828b7f3fa2c526163139bf87f484095a5
julian <julian.anthes@me.com>
2023-06-05 13:13:55 +0200

    change 3 methods

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ def function_1():
     return None
 
 def function_0():
-    ...
+    return None
 
 def refactored(number_1, number_2):
     result = 0
 
     for _ in range(method_4(number_1, number_2)):    
-        number_3 = method_8()
-        result += method_7(result, number_3)
+        number_4 = method_8()
+        result += method_7(result, number_4)
     
     result = method_9(number_2, number_1) - method_6(result, number_1)
 
     return result
 
 def main():
-    print("hello world!")
+    print("Hello world!")
 
 if __name__ == "__main__":
     main()
